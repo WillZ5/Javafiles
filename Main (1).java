45494adca65c306ea9093d13b1f7253b6f8b167d
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-09-28 14:25:12 -0400

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+    Scanner input= new Scanner(System.in);
+      System.out.print("Enter your name");
+    String name = input.nextLine();
+    //String name = input.next();
+    //int name =input.nextInt();
+    System.out.println("My name is "+name);
+
+    Scanner input1 = new Scanner(System.in);
+
+    System .out .println("Enter an integer");
+    int num1=input1.nextInt();
+  
+
+
+
+ }
+}
