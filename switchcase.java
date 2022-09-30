c8a7a8488dbaa5a32001f1838cdbbde91fda7ef0
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-09-29 20:59:03 -0400

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+
+    int a = 10;
+    // Evaluating the expression that will return true or false
+    switch (a) {
+      case 1:
+        System.out.println("Value of a: 1");
+        break;
+      case 5:
+        System.out.println("Value of a: 5");
+        break;
+      case 10:
+        System.out.println("Value of a: 10");
+        break;
+      default:
+        System.out.println("else block");
+        break;
+    }
+  }
+}
