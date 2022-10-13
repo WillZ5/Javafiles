c540b3c21f99179512d3b428eabf3f33602cbfa2
Sshiril <113382540+Sshiril@users.noreply.github.com>
2022-10-13 13:29:44 -0400

    Create even.java

---
+++ even.java
@@ -0,0 +1,15 @@
+//Sum of first n numbers
+class Main {
+  public static void main(String[] args) {
+
+  int i=2; //intialization
+
+    System.out.println("even numbers");
+    while(i<40){
+      System.out.println(i);
+     //i++; //i=i+1;
+      i=i+2;
+    }
+  
+  }
+}
