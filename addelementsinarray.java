7503eaf1a18c613f90bc6e36484d244e1bafa30a
Hti0 <94490841+WillZ5@users.noreply.github.com>
2022-10-27 14:05:13 -0400

    Update addelementsinarray.java

--- addelementsinarray.java
+++ addelementsinarray.java
@@ -1,29 +1,56 @@
-//Program to add eelements in an array
-public class NewMain3 {
+import java.util.Scanner;
 
+public class AlgorithmClass {
     public static void main(String[] args) {
-    
-        //Initialize array  
-        int [] arr = new int [] {1, 2, 3, 4, 5};  
-        //int [] arr = {1,2,3,4,5};
-       System.out.println(arr.length); //length function is used to find the length of array.
-        int sum = 0;  
-        //Loop through the array to calculate sum of elements  
-        for (int i = 0; i < arr.length; i++) {  
-            System.out.println(arr[i]);
-           sum = sum + arr[i];  
-        }  
-        System.out.println("Sum of all the elements of an array: " + sum);  
-    }  
-}  
- /*  Evaluation of for loop
-when i=0 ,  sum = sum + arr[0] = 0+1=1;
-when i=1, sum = sum + arr[1] = 1+2= 3;
-when i=2 , sum =sum + arr[2] = 3+ 3 =6;
-when i=3 , sum = sum + arr[3] = 6+ 4= 10;
- when i=4 , sum = sum + arr[4] = 10+ 5= 15; 
 
 
+        int num;
+        int ii = 0;
+
+        Scanner SetLength = new Scanner(System.in);
+        System.out.print("Enter the array length: ");
+        num = SetLength.nextInt();
+
+        int[] arr = new int[num];
+
+        while ( ii < num){
+
+            System.out.println("Enter value "+ ii + ":" );
+            arr[ii] = SetLength.nextInt();
+            ii++;
+        }
 
 
-*/
+        System.out.println("Array length:" + arr.length);
+        int sum = 0;
+        int MaxNum = arr[0];
+        int MinNum = arr[0];
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.println(arr[i] + " is an even number.");
+            } else {
+                System.out.println(arr[i] + " is an odd number.");
+            }
+
+            if (arr[i] > MaxNum) {
+                MaxNum = arr[i];
+
+            }
+        }
+            System.out.println("The greatest number is: " + MaxNum);
+
+        for (int ii = 0; ii < arr.length; ii++) {
+
+
+            if (arr[ii] < MinNum) {
+                MaxNum = arr[ii];
+
+            }
+        }
+
+    }
+}
+
+
